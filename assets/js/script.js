f1552a9ecc280b53c2d3bb0c8c3a61557a2856b3
rubymaxioned <axioned0482022>
2022-11-22 19:45:36 +0530

    tab functionaliy

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,19 @@
 var tabHeader = document.querySelectorAll('.tabs-head li');
+var tabContent = document.querySelectorAll('.tab-content > li');
+// console.log(tabContent);
+tabHeader[0].classList.add('active');
+tabContent[0].classList.add('show');
+
 console.log(tabHeader);
+console.log(tabContent);
+
+tabHeader.forEach(function(list,index){
+    list.addEventListener('click',function(){
+        for(var i=0; i<tabHeader.length; i++){
+            tabHeader[i].classList.remove('active');
+            tabContent[i].classList.remove('show');
+        }
+        list.classList.add('active');
+        tabContent[index].classList.add('show');
+    })
+})
